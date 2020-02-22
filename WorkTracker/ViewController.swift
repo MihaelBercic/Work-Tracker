39ce308b0b4adff0916f0576440a29a616d3a956
Mihael Berčič <mihael.valentin@gmail.com>
2020-02-22 16:33:27 +0100

    Initial Commit

---
+++ WorkTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WorkTracker
+//
+//  Created by Mihael Bercic on 22/02/2020.
+//  Copyright © 2020 Mihael Bercic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
